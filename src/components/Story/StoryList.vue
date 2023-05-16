957bc27bdb3d9c61bb540c87ce75de068825ab6d
Duy Dat <duydat30122002@gmail.com>
2023-05-17 03:34:38 +0700

    Init Swiper For StoryList

--- src/components/Story/StoryList.vue
+++ src/components/Story/StoryList.vue
@@ -1,25 +1,44 @@
 <template>
-  <div class="story-slide">
-    <swiper
-      :slides-per-view="8"
-      :slides-per-group="4"
-      :space-between="14"
-      :allow-touch-move="true"
-    >
+  <div class="stories-slide">
+    <swiper v-bind="swiperOptions">
       <swiper-slide v-for="n in 20" :key="n">
         <story-item :username="'username' + n" />
       </swiper-slide>
     </swiper>
+    <div class="stories-navigation">
+      <div class="navigation-prev">
+        <fa :icon="['fas', 'angle-right']" />
+      </div>
+      <div class="navigation-next">
+        <fa :icon="['fas', 'angle-left']" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/vue";
 
 import "swiper/css";
 import StoryItem from "./StoryItem.vue";
 
 export default {
+  data() {
+    return {
+      swiperOptions: {
+        modules: [Navigation],
+        slidesPerView: 8,
+        slidesPerGroup: 4,
+        spaceBetween: 14,
+        allowTouchMove: true,
+        navigation: {
+          nextEl: ".navigation-next",
+          prevEl: ".navigation-prev",
+        },
+      },
+    };
+  },
   components: {
     Swiper,
     SwiperSlide,
@@ -29,7 +48,8 @@ export default {
 </script>
 
 <style scoped>
-.story-slide {
+.stories-slide {
+  position: relative;
   width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import router from "./router";
 
 import "@/assets/css/global.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faAngleRight, faAngleLeft);
+
 const app = createApp(App);
+
+app.component("fa", FontAwesomeIcon);
 
 app.directive("click-outside", {
   mounted(el, binding) {
