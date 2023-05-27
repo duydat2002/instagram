6536aca47f4a4a0bfd4a2ae165f98e50dc8bff0b
Duy Dat <duydat30122002@gmail.com>
2023-05-27 10:33:58 +0700

    Splash screen loading when reload page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,41 @@
 <template>
-  <Suspense>
-    <keep-alive>
+  <div>
+    <loading v-if="isLoading" />
+
+    <keep-alive v-else>
       <component :is="$route.meta.layout" />
     </keep-alive>
-
-    <template #fallback>
-      <h1
-        style="
-          font-size: 30px;
-          position: fixed;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          z-index: 1000;
-        "
-      >
-        Loading...
-      </h1>
-    </template>
-  </Suspense>
+  </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import Loading from "./components/Loading.vue";
 
 export default {
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
   computed: {
     ...mapGetters(["user"]),
   },
   methods: {
     ...mapActions(["initUser"]),
+    handleBeforeUnload(event) {
+      this.isLoading = true;
+      event.preventDefault();
+    },
   },
   async mounted() {
+    window.addEventListener("beforeunload", this.handleBeforeUnload);
     await this.initUser();
+    this.isLoading = false;
   },
+  beforeUnmount() {
+    window.removeEventListener("beforeunload", this.handleBeforeUnload);
+  },
+  components: { Loading },
 };
 </script>
---
+++ src/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="splash-screen">
+    <div class="logo">
+      <img src="" alt="Instagram" />
+    </div>
+    <div class="credit">
+      <img src="" alt="Credit" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
